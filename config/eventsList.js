7f289376314c71f75122826a5d2665dd2f445fc8
Inazio Claver <jclaver64@gmail.com>
2022-01-18 16:02:05 +0100

    [Events]. Provisional load in main page with new url

--- config/eventsList.js
+++ config/eventsList.js
@@ -3,26 +3,24 @@ const EVENTS =
 	"0" : 
 	{
 		"name" : "Pajaritas por la ciencia",
-		"preview-img" : "/resources/img/events/pajaritas-por-la-ciencia/preview.jpg",
+		"preview-img" : "https://inazense.github.io/Hubuntu/resources/img/events/pajaritas-por-la-ciencia/preview.jpg",
 		"start-datetime" : "2021-12-14_10:00",
 		"end-datetime" : "2021-12-14_14:00",
 		"enrollment-link" : "https:www.google.com",
 		"carousel" : 
 		{
-			"001" : "/resources/img/events/pajaritas-por-la-ciencia/carousel-1.png",
-			"002" : "/resources/img/events/pajaritas-por-la-ciencia/carousel-2.png",
-			"003" : "/resources/img/events/pajaritas-por-la-ciencia/carousel-3.png"
+			"001" : "https://inazense.github.io/Hubuntu/resources/img/events/pajaritas-por-la-ciencia/carousel-1.png",
+			"002" : "https://inazense.github.io/Hubuntu/resources/img/events/pajaritas-por-la-ciencia/carousel-2.png",
+			"003" : "https://inazense.github.io/Hubuntu/resources/img/events/pajaritas-por-la-ciencia/carousel-3.png"
 		},
 	},
 	"1" : 
 	{
 		"name" : "Test event",
-		"preview-img" : "/resources/img/events/test/preview.jpg",
+		"preview-img" : "https://inazense.github.io/Hubuntu/resources/img/events/test/preview.png",
 		"start-datetime" : "2022-01-31_10:00",
 		"end-datetime" : "2022-01-31_14:00",
 		"enrollment-link" : "https:www.google.com",
 		"carousel" : {}
 	},
 };
-
-console.log(EVENTS);
